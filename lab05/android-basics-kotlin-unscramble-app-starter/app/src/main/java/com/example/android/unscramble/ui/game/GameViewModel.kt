a4ea1bb288d86c2dbc27de674c7f9384c60f82f2
guanyiming <2818067461@qq.com>
2022-03-30 09:52:27 +0800

    lab05

---
+++ lab05/android-basics-kotlin-unscramble-app-starter/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,80 @@
+package com.example.android.unscramble.ui.game
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+
+    private val _score = MutableLiveData(0)
+    val score:LiveData<Int>
+        get() = _score
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
+        get() = _currentWordCount
+    private val _currentScrambledWord = MutableLiveData<String>()
+    val currentScrambledWord: LiveData<String>
+        get() = _currentScrambledWord
+
+    private var wordsList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord() {
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+        while (String(tempWord).equals(currentWord, false)) {
+            tempWord.shuffle()
+        }
+        if (wordsList.contains(currentWord)){
+            getNextWord()
+        } else{
+            _currentScrambledWord.value = String(tempWord)
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
+            wordsList.add(currentWord)
+        }
+    }
+    init {
+        Log.d("GameFragment", "GameViewModel created!")
+        getNextWord()
+    }
+    /*
+* Returns true if the current word count is less than MAX_NO_OF_WORDS.
+* Updates the next word.
+*/
+    fun nextWord(): Boolean {
+        return if (currentWordCount.value!! < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
+
+    private fun increaseScore() {
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
+    }
+
+    //在 isUserWordCorrect() 中，确认玩家猜出的单词是否正确，如果正确就增加得分。这将更新提醒对话框中的最终得分。
+    fun isUserWordCorrect(playerWord: String): Boolean {
+        if (playerWord.equals(currentWord, true)) {
+            increaseScore()
+            return true
+        }
+        return false
+    }
+
+    /*
+    * Re-initializes the game data to restart the game.
+    */
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
+        wordsList.clear()
+        getNextWord()
+    }
+
+    //在 ViewModel 被销毁前会调用 onCleared() 回调
+    override fun onCleared() {
+        super.onCleared()
+        Log.d("GameFragment", "GameViewModel destroyed!")
+    }
+}
