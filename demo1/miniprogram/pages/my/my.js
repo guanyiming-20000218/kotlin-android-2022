fa2005b4c8c07328c59ff00ec4c94b0e7553b646
guanyiming <2818067461@qq.com>
2022-05-19 21:51:46 +0800

    我的页面与登录功能

--- demo1/miniprogram/pages/my/my.js
+++ demo1/miniprogram/pages/my/my.js
@@ -1,4 +1,5 @@
 // miniprogram/pages/my/my.js
+const db = wx.cloud.database()
 Page({
 
     /**
@@ -8,11 +9,60 @@ Page({
         show : true
     },
 
+    // 注册
+    register(e){
+        let that = this
+        wx.showModal({
+            title: '提示',
+            content: '您还未注册，是否注册',
+            success(res){
+                if(res.confirm){
+                    console.log('用户点击确定')
+                    wx.showLoading({
+                      title: '注册中',
+                    })
+                    wx.getUserProfile({
+                        desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+                        success: (userInfo) => {
+                            db.collection('user').add({
+                                data:{
+                                    userInfo:userInfo.userInfo
+                                }
+                            }).then(user=>{
+                                wx.hideLoading()
+                                wx.showToast({
+                                  title: '注册成功',
+                                })
+                                that.login()
+                            })
+
+                        }
+                    })
+                }else if(res.cancel){
+                  console.log('用户点击取消')
+                }
+            }
+        })
+    },
+    // 登录
+    login(){
+        let that = this
+        db.collection('user').get().then(res=>{
+            if(res.data.length > 0){
+                that.setData({
+                    user:res.data[0]
+                })
+            }else{
+                that.register()
+            }
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        let that = this
+        that.login()
     },
 
     /**
